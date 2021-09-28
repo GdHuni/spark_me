a7f57b06cf64440cb439510935c243ad9b681502
“周虎” <zhouhu@leyoujia.com>
2021-09-28 19:02:10 +0800

    rdd df ds 之间的转换

--- src/main/scala/com/huni/spark/spark_sql/DsDfDemo.scala
+++ src/main/scala/com/huni/spark/spark_sql/DsDfDemo.scala
@@ -1,7 +1,7 @@
 package com.huni.spark.spark_sql
 
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
 import org.apache.spark.sql.functions.desc
 import org.apache.spark.sql.types.StructType
 
@@ -52,11 +52,16 @@ object DsDfDemo {
 
     //==============================通过集合创建ds=====================================
     val persons = List(Person("zhan", 18, 10), Person("lisi", 19, 22))
-    val rdd: RDD[Person] = sc.makeRDD(persons)
-    val ds: Dataset[Person] = rdd.toDS()
-    val ds1 = spark.createDataset(persons)
-    val ds2 = spark.createDataset(rdd)
+    val ds = spark.createDataset(persons)
     ds.show
+
+
+    // Dataset = RDD[case class]  DataFrame = RDD[Row] + Schema
+    //================================RDD转Dataset================================
+    val rdd: RDD[Person] = sc.makeRDD(persons)
+    // 反射推断，spark 通过反射从case class的定义得到类名
+    val ds1: Dataset[Person] = rdd.toDS()
+    val ds2 = spark.createDataset(rdd)
     ds1.show
     ds2.show
 
@@ -76,9 +81,62 @@ object DsDfDemo {
     frame.orderBy("age").show(10)
 
 
+    //==============================RDD 转成 DataFrame=====================================
+    val arr = Array(("Jack", 28, 184), ("Tom", 10, 144), ("Andy", 16, 165))
 
+    //创建RDD[Row]
+    val rdd3: RDD[Row] = sc.makeRDD(arr).map(r => Row(r._1, r._2, r._3))
 
+    //创建schema,名称、类型、是否可以为nul
+    val schema3: StructType = new StructType()
+      .add("name", "string",false)
+      .add("age", "int",false)
+      .add("height", "int",false)
 
+    val df3: DataFrame = spark.createDataFrame(rdd3, schema3)
+    df3.orderBy(desc("name")).show()
+    println("============")
+
+    //非 RDD[Row]的RDD可以 调用todDF函数 import spark.implicits._ 隐式转换里的
+    val rdd4: RDD[(String, Int, Int)] = sc.makeRDD(arr)
+    // 反射推断
+    rdd4.toDF("name4","age4","height4").show()
+
+    //==============================从文件创建DateFrame(以csv文件为例)=====================================
+    //默认的,分割 不加头信息(第一行是否作为列名)
+    val df5: DataFrame = spark.read
+      .csv("data/people1.csv")
+    df5.printSchema()
+    df5.show()
+    // 指定参数  spark 2.3.0以上才行
+    val schema6 = "name string, age int, job string"
+    val df6: DataFrame = spark.read
+      .option("header","true")
+      .option("delimiter",";")
+      .schema(schema6)
+      .csv("data/people2.csv")
+    df6.printSchema()
+    df6.show()
+
+    // 自动类型推断
+    val df7: DataFrame = spark.read
+      .option("header","true")
+      .option("delimiter",";")
+      .option("inferschema","true")
+      .csv("data/people2.csv")
+    df7.printSchema()
+    df7.show()
+
+    //===============================df、ds、rdd转换关系===============================
+    //ds ==> df,rdd
+    ds.toDF()
+    ds.rdd
+    //df ==> ds,rdd
+    case class Coltest(col1:String,col2:Int)extends Serializable //定义字段名和类型
+    df7.as[Coltest]
+    df7.rdd
+
+    //rdd ==> ds,df //Dataset = RDD[case class]  DataFrame = RDD[Row] + Schema
     //关闭资源
     spark.close()
 
